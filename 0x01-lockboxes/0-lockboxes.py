61b20841636a0b77fb3a51f4ee05c13060d45622
Amr Elstohy <amramr2901@gmail.com>
2024-10-11 02:35:44 +0300

    hi

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -9,16 +9,21 @@ def canUnlockAll(boxes):
         return False
 
     keys = boxes[0]
+    num_boxes = len(boxes)
 
     if 0 not in boxes:
         keys.append(0)
 
     for key in keys:
+        if key > num_boxes:
+            keys.remove(key)
+            continue
+
         for k in boxes[key]:
             if k not in keys:
                 keys.append(k)
 
-    if len(keys) == len(boxes):
+    if len(keys) == num_boxes:
         return True
     else:
         return False
