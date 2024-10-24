e86741a23257a9831b3c7800d8fd42538a91d7ad
Amr Elstohy <amramr2901@gmail.com>
2024-10-24 17:16:28 +0300

    hi

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
 import sys
+import re
 
+
+log_pattern = re.compile(
+    r'(\d{1,3}(?:\.\d{1,3}){3}) - \[(.*?)\] '
+    r'"GET /projects/260 HTTP/1\.1" (200|301|400|401|403|404|405|500) (\d+)'
+)
 for line in sys.stdin:
-    print(line.strip())
+    match = log_pattern.search(line.strip())
+    if match:
+        print(match)
+    else:
+        print('oops')
 
